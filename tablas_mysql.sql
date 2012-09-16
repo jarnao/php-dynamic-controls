2597bf396fb4ec76bd67a8b1e7f29873ddc09e7d
José Arnao <jarnao@msn.com>
2012-09-16 12:30:40 -0500

    Datos de MySQL

--- tablas_mysql.sql
+++ tablas_mysql.sql
@@ -25,3 +25,18 @@ CREATE TABLE `promedios` (
 `promedio` int(11) DEFAULT NULL,
 PRIMARY KEY (`idpromedio`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8$$
+
+
+
+INSERT INTO `calificacion` (`idcalificacion`, `nombre_calificacion`, `peso`) VALUES (1,'P1',10);
+INSERT INTO `calificacion` (`idcalificacion`, `nombre_calificacion`, `peso`) VALUES (2,'P2',20);
+INSERT INTO `calificacion` (`idcalificacion`, `nombre_calificacion`, `peso`) VALUES (3,'P3',30);
+INSERT INTO `calificacion` (`idcalificacion`, `nombre_calificacion`, `peso`) VALUES (4,'P4',40);
+
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000001','Jose Arnao');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000002','Leticia Sucacahua');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000003','Talia Quispe');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000004','William Saman');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000005','Paul Sanchez');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000006','Giovani Simon');
+INSERT INTO `estudiante` (`coduniv`, `nombre_completo`) VALUES ('0000000007','Carmen Aliaga');
